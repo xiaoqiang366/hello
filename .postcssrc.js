afef9daf1b150cae36fe94f38357e2263e36c4eb
敖晓强 <aoxiaoqiang@163.com>
2018-06-13 14:52:15 +0800

    feat: 配置修改

---
+++ .postcssrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {
+      browsers: ['android >= 4.3', 'ios >= 6.0']
+    }
+  }
+}
